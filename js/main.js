12b3ee078523ce235fe8be983c69c6cdece58afb
lx1823616178 <lx1823616178@outlook.com>
2020-06-02 16:28:07 +0800

    兼容窗口

--- js/main.js
+++ js/main.js
@@ -5,4 +5,28 @@ $(function () {
     bottomCenterCharts()
     getBottomLeftcharts()
     getBottomRightCharts()
+    resize()
 })
+
+function resize() {
+    window.onresize = function () {
+        adjustWindowZoom()
+    };
+
+    if (document.createEvent) {
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent("resize", true, true);
+        window.dispatchEvent(event);
+    } else if (document.createEventObject) {
+        window.fireEvent("onresize");
+    }
+}
+
+function adjustWindowZoom() {
+    $('.contain')[0].style.zoom = window.innerWidth / 1920
+    $('body').height('1080px')
+    $(function () {
+        setInterval('$("#date_number").html(getTime())', 1000)
+    })
+
+}
